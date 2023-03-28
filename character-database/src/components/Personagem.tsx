64f9bebc3f48243f810d4329c9beaa46fdb4b9f8
ssouza0001 <ssouza0001@outlook.com>
2023-03-28 19:32:59 -0300

    convertido a pesquisa de personagem de axio para graphql

--- character-database/src/components/Personagem.tsx
+++ character-database/src/components/Personagem.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { getPersonagemPorNome } from "../services/GetPersonagemPorNome";
+import { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_PERSONAGEM_POR_NOME } from "../services/GetPersonagemPorNome";
 
 type PersonagemType = {
   name: string;
@@ -8,16 +9,28 @@ type PersonagemType = {
 
 function Personagem() {
   const [personagem, setPersonagem] = useState("");
+  const { loading, error, data } = useQuery(GET_PERSONAGEM_POR_NOME, {
+    variables: { nome: personagem },
+  });
   const [dados, setDados] = useState<PersonagemType | null>(null);
 
-  async function handlePesquisar() {
+  const handlePesquisar = async () => {
     try {
-      const response = await getPersonagemPorNome(personagem);
-      setDados(response);
+      await new Promise((resolve) => setTimeout(resolve, 1000)); // Simulando um delay
+
+      if (data && data.characters && data.characters.results) {
+        const { results } = data.characters;
+        const personagem = results[0];
+
+        setDados({
+          name: personagem.name,
+          image: personagem.image,
+        });
+      }
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   return (
     <div>
@@ -30,6 +43,8 @@ function Personagem() {
       />
       <button onClick={handlePesquisar}>Pesquisar</button>
 
+      {loading && <p>Carregando...</p>}
+      {error && <p>Ocorreu um erro.</p>}
       {dados && (
         <div>
           <h2>{dados.name}</h2>
--- character-database/src/services/GetPersonagemPorNome.tsx
+++ character-database/src/services/GetPersonagemPorNome.tsx
@@ -1,8 +1,34 @@
-import axios from "axios";
+import { useQuery, gql } from "@apollo/client";
 
-const baseURL = "https://rickandmortyapi.com/api";
+export const GET_PERSONAGEM_POR_NOME = gql`
+  query GetPersonagemPorNome($nome: String!) {
+    characters(filter: { name: $nome }) {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
 
-export const getPersonagemPorNome = async (nome: string) => {
-  const response = await axios.get(`${baseURL}/character/?name=${nome}`);
-  return response.data.results[0];
+const Personagem = ({ nome }: { nome: string }) => {
+  const { loading, error, data } = useQuery(GET_PERSONAGEM_POR_NOME, {
+    variables: { nome },
+  });
+
+  if (loading) return <p>Carregando...</p>;
+  if (error) return <p>Ocorreu um erro.</p>;
+  if (!data?.characters?.results[0]) return <p>Nenhum resultado encontrado.</p>;
+
+  const personagem = data.characters.results[0];
+
+  return (
+    <div>
+      <h2>{personagem.name}</h2>
+      <img src={personagem.image} alt={nome} />
+    </div>
+  );
 };
+
+export default Personagem;
